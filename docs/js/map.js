c12adb201f6fc6ceae2e792ea32d2ea3bf99ec6b
Jacob Farkas <15792629+jacobfarkas@users.noreply.github.com>
2026-03-24 19:35:12 -0400

    Increase zIndex for ntaPane to 250

--- docs/js/map.js
+++ docs/js/map.js
@@ -50,7 +50,7 @@ map.getPane('parksPane').style.zIndex = 450;
 
 // Custom pane for NTA - below hex, pointer events off when hex visible
 map.createPane('ntaPane');
-map.getPane('ntaPane').style.zIndex = 200;
+map.getPane('ntaPane').style.zIndex = 250;
 map.getPane('ntaPane').style.pointerEvents = 'none';
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
