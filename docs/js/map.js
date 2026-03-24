5ef9cacff270f9e729dc1755c657b4a74eabaebc
Jacob Farkas <15792629+jacobfarkas@users.noreply.github.com>
2026-03-23 21:06:16 -0400

    Update fetch paths for GeoJSON data files

--- docs/js/map.js
+++ docs/js/map.js
@@ -117,7 +117,7 @@ function golfPopup(p) {
 // -----------------------------------------------------------------------------
 
 // 1. NTA scores - neighborhood level, semi-transparent fill
-fetch('../data-prep/processed/nta_scores.geojson')
+fetch('../data/nta_scores.geojson')
   .then(function(r) { return r.json(); })
   .then(function(data) {
 
@@ -137,7 +137,7 @@ fetch('../data-prep/processed/nta_scores.geojson')
     }).addTo(ntaLayer);
 
     // 2. Hex scores - cell level, on top of NTA
-    return fetch('../data-prep/processed/hex_scores_parks.geojson');
+    return fetch('../data/hex_scores_parks.geojson');
   })
   .then(function(r) { return r.json(); })
   .then(function(data) {
@@ -158,7 +158,7 @@ fetch('../data-prep/processed/nta_scores.geojson')
     }).addTo(hexLayer);
 
     // 3. Parks display - solid dark green, on top of hex
-    return fetch('../data-prep/processed/parks_display.geojson');
+    return fetch('../data/parks_display.geojson');
   })
   .then(function(r) { return r.json(); })
   .then(function(data) {
@@ -176,7 +176,7 @@ fetch('../data-prep/processed/nta_scores.geojson')
     }).addTo(parksLayer);
 
     // 4. Golf courses - pale green overlay, on top of parks
-    return fetch('../data-prep/raw/golf_courses.geojson');
+    return fetch('../data/golf_courses.geojson');
   })
   .then(function(r) { return r.json(); })
   .then(function(data) {
